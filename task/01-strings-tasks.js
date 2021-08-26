5233c7360a7ef1229615044615dd5eb12b454073
Glib13 <quldi410@gmail.com>
2021-08-26 17:10:16 +0300

    done function extractNameFromTemplate

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -65,7 +65,7 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
 function extractNameFromTemplate(value) {
-	throw new Error('Not implemented');
+	return value.substr(7).replace('!','')
 }
 
 /**
