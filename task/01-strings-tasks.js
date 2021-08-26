f927fd2d98224f39474f38042d2146a2dc7587e4
Glib13 <quldi410@gmail.com>
2021-08-26 17:19:12 +0300

    done removeFirstOccurrences

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -125,7 +125,7 @@ function repeatString(value, count) {
  *   'ABABAB','BA' => 'ABAB'
  */
 function removeFirstOccurrences(str, value) {
-	throw new Error('Not implemented');
+	return str.replace(value, '');
 }
 
 /**
