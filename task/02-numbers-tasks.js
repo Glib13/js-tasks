f5ec90dd0b57340d7c50eb8136db88444f37896b
Glib13 <quldi410@gmail.com>
2021-09-02 14:19:42 +0300

    feat: done isPrime

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -199,7 +199,9 @@ function roundToPowerOfTen(num, pow) {
  *   17 => true
  */
 function isPrime(n) {
-	throw new Error('Not implemented');
+	for (const i = 0; i < n; i+1)
+		if(n % i === 0) return false;
+	return n > 1;
 }
 
 /**
