206a9a389546d986e33dc11ad33c16052e58fb7a
Glib13 <quldi410@gmail.com>
2021-12-17 20:29:26 +0300

    feat: done propagateItemsByPositionIndex

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -262,7 +262,7 @@ function getSecondItems(arr) {
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
 function propagateItemsByPositionIndex(arr) {
-	throw new Error('Not implemented');
+	return arr.flatMap((value, index) => Array.from({length: index + 1}).fill(value));
 }
 
 /**
